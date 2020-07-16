02619309aa3887935c5bb0b366da403401e12ae9
Jillian Duma <jduma108@gmail.com>
2020-07-16 11:05:12 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('Client side js file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) =>  {
+    event.preventDefault()
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    
+    fetch ('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return messageOne.textContent = 'Unable to find search. Please try another location.'
+            }
+            messageOne.textContent = data.location 
+            messageTwo.textContent = data.forecast 
+        })
+    })
+})      
